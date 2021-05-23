733390592c41f8980e72611f07e28406380914e2
Orlando-pt <orlando.macedo15@ua.pt>
2021-05-23 11:06:29 +0100

    start spring boot application

---
+++ WebMarket/src/test/java/deti/tqs/WebMarket/WebMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package deti.tqs.WebMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
