fd646f67c99828068f33523d22241be95b7e9dbf
Orlando-pt <orlando.macedo15@ua.pt>
2021-05-29 13:29:11 +0100

    User model with DataJpaTest

--- webmarket/src/main/java/deti/tqs/webmarket/model/User.java
+++ webmarket/src/main/java/deti/tqs/webmarket/model/User.java
@@ -1,18 +1,11 @@
 package deti.tqs.webmarket.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -21,14 +14,109 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
+    @Email
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(columnDefinition = "VARCHAR(20) CHECK (role IN ('ADMIN', 'RIDER', 'CUSTOMER'))")
     private String role;
 
     // TODO encrypt password
+    @Column(nullable = false)
     private String password;
 
+    @Size(min = 9, max = 9)
+    @Column(length = 9)
     private String phoneNumber;
+
+    /** we have to put this boiler plate code because of tests made with DataJpaTest
+     *  lombok code is only available after the start of the application
+     *  and DataJpaTest doesn't fully start the application
+      */
+
+    public User() {}
+
+    public User(String username, String email, String role, String password, String phoneNumber) {
+        this.username = username;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(role, user.role) && Objects.equals(password, user.password) && Objects.equals(phoneNumber, user.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, role, password, phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
