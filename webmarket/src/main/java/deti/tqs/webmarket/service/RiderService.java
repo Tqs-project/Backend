f1282b139142f7476fd49e97e4346fcee005c7a2
Rui-FMF <ruifmf2000@gmail.com>
2021-06-07 13:14:22 +0100

    fixed imports

--- webmarket/src/main/java/deti/tqs/webmarket/service/RiderService.java
+++ webmarket/src/main/java/deti/tqs/webmarket/service/RiderService.java
@@ -1,10 +1,28 @@
 package deti.tqs.webmarket.service;
 
 
+import deti.tqs.webmarket.dto.RiderDto;
+import deti.tqs.webmarket.model.Rider;
+import deti.tqs.webmarket.repository.RiderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class RiderService {
     
     @Autowired
 	private RiderRepository repository;
+
+    public Rider registerRider(RiderDto riderDto){
+
+    }
+
+    public Object login(String email, String password){
+
+    }
+
+    public Rider getRiderByEmail(String email) {
+
+    }
 
 }
