47b78f409fcb60b28b0b76e980a0d4dd3a95987a
Orlando-pt <orlando.macedo15@ua.pt>
2021-06-18 12:29:11 +0100

    steel from the last commit

---
+++ webmarket/src/main/java/deti/tqs/webmarket/dto/RiderFullInfoDto.java
@@ -0,0 +1,34 @@
+package deti.tqs.webmarket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RiderFullInfoDto {
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String role;
+
+    private String phoneNumber;
+
+    private String vehiclePlate;
+
+    private List<Long> comments;
+
+    private String lat;
+
+    private String lng;
+
+    private Boolean busy;
+
+    private List<Long> rides;
+}
