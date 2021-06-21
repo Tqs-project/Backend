75cc62de8ee143cfbae056bcde1f4967a20527e6
Orlando-pt <orlando.macedo15@ua.pt>
2021-06-21 23:52:00 +0100

    add admin frontend origin

--- webmarket/src/main/java/deti/tqs/webmarket/controller/AdminController.java
+++ webmarket/src/main/java/deti/tqs/webmarket/controller/AdminController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.Queue;
 
 @Log4j2
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","https://wedeliveradmin.herokuapp.com"})
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
